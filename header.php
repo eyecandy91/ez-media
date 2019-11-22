3a4239ee187b67e2caee94aa3c8cf438a38b944c
Henry puttick <hputtick@me.com>
2019-11-22 16:19:42 +0700

    header code added

--- header.php
+++ header.php
@@ -17,8 +17,13 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    
+    <?php wp_head();
 
-    <?php wp_head(); ?>
+    if (get_field('header_code')) :
+        $other_page = 4;
+        the_field('header_code', $other_page); 
+    endif;?>
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
